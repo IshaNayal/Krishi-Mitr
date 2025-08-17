e25ac22e35d87a83e67232cff7281b9d2a30970f
Isha Nayal <ishanayal16@gmail.com>
2025-08-17 18:28:36 +0530

    Create cropData.js

---
+++ cropData.js
@@ -0,0 +1,19 @@
+// cropData.js
+export const cropData = {
+  wheat: { yieldPerAcre: 2.5, rainfallNeeded: 500, fertilizerNeeded: 50 },
+  rice: { yieldPerAcre: 3.8, rainfallNeeded: 1200, fertilizerNeeded: 80 },
+  maize: { yieldPerAcre: 2.2, rainfallNeeded: 600, fertilizerNeeded: 40 },
+  sugarcane: { yieldPerAcre: 8.5, rainfallNeeded: 1500, fertilizerNeeded: 120 },
+  barley: { yieldPerAcre: 2.0, rainfallNeeded: 400, fertilizerNeeded: 40 },
+  millet: { yieldPerAcre: 1.8, rainfallNeeded: 350, fertilizerNeeded: 30 },
+  sorghum: { yieldPerAcre: 2.0, rainfallNeeded: 450, fertilizerNeeded: 35 },
+  pulses: { yieldPerAcre: 1.0, rainfallNeeded: 400, fertilizerNeeded: 20 },
+  cotton: { yieldPerAcre: 1.5, rainfallNeeded: 700, fertilizerNeeded: 60 },
+  groundnut: { yieldPerAcre: 1.8, rainfallNeeded: 500, fertilizerNeeded: 25 },
+  soybean: { yieldPerAcre: 2.0, rainfallNeeded: 600, fertilizerNeeded: 40 },
+  sunflower: { yieldPerAcre: 1.6, rainfallNeeded: 550, fertilizerNeeded: 35 },
+  potato: { yieldPerAcre: 6.0, rainfallNeeded: 700, fertilizerNeeded: 100 },
+  onion: { yieldPerAcre: 5.0, rainfallNeeded: 650, fertilizerNeeded: 90 },
+  tomato: { yieldPerAcre: 4.5, rainfallNeeded: 600, fertilizerNeeded: 80 },
+};
+
