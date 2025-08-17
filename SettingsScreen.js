2470282dde5ec758de48b4e1eed49544bd9b0830
Isha Nayal <ishanayal16@gmail.com>
2025-08-17 13:27:06 +0530

    Create SettingsScreen.js

---
+++ SettingsScreen.js
@@ -0,0 +1,51 @@
+//SettingsScreen.js
+import React from 'react';
+import { View, Text, StyleSheet,SafeAreaView, ImageBackground } from 'react-native';
+
+export default function SettingsScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+          <View style={styles.header}>
+            <Text style={styles.title}>Settings</Text>
+          </View>
+          <View style={styles.content}>
+            <Text style={styles.text}>Welcome to Settings</Text>
+          </View>
+        </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#b4aaaaff',
+  },
+  header: {
+    paddingVertical: 20,
+    paddingHorizontal: 15,
+    backgroundColor: '#424242',
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#fff',
+    textAlign: 'center',
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  text: {
+    fontSize: 18,
+    color: '#333',
+    textAlign: 'center',
+  },
+});
+
+
+
